0d809497bdb7a880f76b506c285197572df82411
assaffmatias <matias.assaff98@gmail.com>
2024-02-05 15:26:29 -0300

    responsive design

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -14,26 +14,25 @@ export default function Nav({ onSearch }) {
     return (
         <nav className={style.nav}>
             <div className={style.right}>
-                <Link to={PATHROUTES.FAVORITES}>
-                    <button className={style.button}>Favorites</button>
-                </Link>
-                <Link to={PATHROUTES.ABOUT}>
-                    <button className={style.button}>About</button>
-                </Link>
-            </div>
-
-            <div>
-                    <img src={logo} alt="" className={style.img} />
-            </div>
-
-            <div className={style.button_nav}>
                 <Link to={PATHROUTES.HOME}>
                     <button className={style.button}>Home</button>
                 </Link>
                 <Link to={PATHROUTES.CHARACTERS}>
                     <button className={style.button}>Characters</button>
                 </Link>
+            </div>
 
+            <div>
+                <img src={logo} alt="" className={style.img} />
+            </div>
+
+            <div className={style.left}>
+                <Link to={PATHROUTES.FAVORITES}>
+                    <button className={style.button}>Favorites</button>
+                </Link>
+                <Link to={PATHROUTES.ABOUT}>
+                    <button className={style.button}>About</button>
+                </Link>
             </div>
         </nav>
     )
